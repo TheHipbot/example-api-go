3dc7fc57a011105aafb465be7f09b3528708d5d6
Jeremy Chambers <jeremy@thehipbot.com>
2017-03-28 21:07:09 -0500

    Updates to use latest version of thehipbot/golang:1.8-onbuild

--- build.sh
+++ build.sh
@@ -7,6 +7,6 @@ if [ ! -d "$(pwd)/build" ]; then
 fi
 
 docker build -t thehipbot/example-api-go-build:$GIT_COMMIT -f Dockerfile.build --build-arg PACKAGE=github.com/TheHipbot/example-api-go .
-docker run --rm -v $(pwd)/build:/build thehipbot/example-api-go-build:$GIT_COMMIT
+docker run --rm -v $(pwd)/build:/host/build thehipbot/example-api-go-build:$GIT_COMMIT
 docker image rm thehipbot/example-api-go-build:$GIT_COMMIT
 docker build -t thehipbot/example-api-go:$GIT_COMMIT -f Dockerfile.run .
